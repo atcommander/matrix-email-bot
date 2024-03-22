1024e8a92837e8f6351e42409292fcb2b66a243b
Dark Decoy <darkdecoy@notracking.space>
2024-03-22 22:07:50 +0000

    Updating log messages in the process function to start with the message id of email that is being processed

--- src/EmailProcessor.ts
+++ src/EmailProcessor.ts
@@ -41,7 +41,7 @@ export class EmailProcessor {
 
     public async processMessage(message: any) {
         if (await this.db.doesMessageExist(message.messageId)) {
-            console.log("Message Already Processed");
+            console.log(message.messageId + ": " + "Message Already Processed");
             return;
         }
 
@@ -54,9 +54,9 @@ export class EmailProcessor {
         const primaryFrom = message.from.value[0];
 
         const rooms: string[] = [];
-        console.log("Processing targets...");
+        console.log("message.messageId + ": " + Processing targets...");
         for (const target of targets) {
-            console.log("Processing Target ", target.address, "...");
+            console.log("message.messageId + ": " + Processing Target ", target.address, "...");
             if (!target.address) continue;
 
             const roomConfigs = getRoomConfigsForTarget(target.address, target.source);
@@ -87,7 +87,7 @@ export class EmailProcessor {
                         if (!fromAddress.address) continue;
 
                         if (!roomConfig.allowedSenders.includes(fromAddress.address.toLowerCase())) {
-                            console.log(fromAddress.address, ' is not in Alllowed Senders List')
+                            console.log(message.messageId + ": " + fromAddress.address, ' is not in Alllowed Senders List')
                             allowed = false;
                             break;
                         }
@@ -98,7 +98,7 @@ export class EmailProcessor {
                     if (!fromAddress.address) continue;
 
                     if (roomConfig.blockedSenders.includes(fromAddress.address.toLowerCase())) {
-                        console.log(fromAddress.address, ' is in Blocked Senders List')
+                        console.log(message.messageId + ": " + fromAddress.address, ' is in Blocked Senders List')
                         allowed = false;
                         break;
                     }
@@ -188,40 +188,40 @@ export class EmailProcessor {
                             this.burststart = new Date().getTime();
                         }
 
-                        console.log("Waiting for " + this.waittime + " Ms...");
+                        console.log(message.email_id + ": " + "Waiting for " + this.waittime + " Ms...");
                         await new Promise(f => setTimeout(f, this.waittime));
 
-                        console.log('...Try #' + messageRetries + ' of Message ' + message.email_id);
+                        console.log(message.email_id + ": " + '...Try #' + messageRetries + ' of Message ' + message.email_id);
                         messageStatus = await this.bot.sendMessage(msg, roomConfig.roomId, msgType);
 
                         if (messageStatus.retryAfterMs > this.waittime) {
-                            console.log('Increasing Wait Time to ' + messageStatus.retryAfterMs)
+                            console.log(message.email_id + ": " + 'Increasing Wait Time to ' + messageStatus.retryAfterMs)
                             this.waittime = messageStatus.retryAfterMs;
                         }
 
                         let elapsed = new Date().getTime() - this.burststart;
-                        console.log("Burst Length: " + elapsed + " milliseconds");
+                        console.log(message.email_id + ": " + "Burst Length: " + elapsed + " milliseconds");
 
                         if (elapsed > config.matrix.burst.length ) {
                             this.burstcount = 0;
                         }
 
                         if (this.burstcount >= config.matrix.burst.messageThreshold) {
-                            console.log("Burst Message Threshold Hit at " + this.burstcount + "/" + config.matrix.burst.messageThreshold);
-                            console.log("Adding " + config.matrix.burst.waitTime + " milliseconds to wait time between message tries");
+                            console.log(message.email_id + ": " + "Burst Message Threshold Hit at " + this.burstcount + "/" + config.matrix.burst.messageThreshold);
+                            console.log(message.email_id + ": " + "Adding " + config.matrix.burst.waitTime + " milliseconds to wait time between message tries");
                             this.waittime = this.waittime + config.matrix.burst.waitTime;
                         }
 
-                        console.log(messageStatus.statusCode + ' ' + messageStatus.message)
+                        console.log(message.email_id + ": " + "Status Code: " + messageStatus.statusCode + ' Message: ' + messageStatus.message)
                     } while (messageStatus.statusCode != 200 && messageRetries < config.matrix.messageTries);
 
                     if (messageStatus.statusCode == 200) {
                         this.waittime = 0;
-                        console.log('Message Sent from ' + msg.from_email);
+                        console.log(message.email_id + ": " + 'Message Sent from ' + msg.from_email);
                     }
                     else {
-                        console.log('Message Failed after ' + messageRetries + ' Tries');
-                        console.log('From: ' + msg.from_email + ' Subject: ' + msg.subject + ' Date: ' + msg.date);
+                        console.log(message.email_id + ": " + 'Message Failed after ' + messageRetries + ' Tries');
+                        console.log(message.email_id + ": " + 'From: ' + msg.from_email + ' Subject: ' + msg.subject + ' Date: ' + msg.date);
                         this.waittime = this.waittime + config.matrix.failedWaitTime
                     }                        
 
